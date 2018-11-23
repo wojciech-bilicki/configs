81627dad706851a11327651fb53c884e1a881e9e
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2018-11-23 14:48:57 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+// For more information on customizing Oni,
+// check out our wiki page:
+// https://github.com/onivim/oni/wiki/Configuration
+
+const activate = oni => {
+    console.log("config activated")
+
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<f11>", "oni.debug.openDevTools")
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+    //
+    // Or remove the default bindings:
+    //
+    // oni.input.unbind("<c-p>")
+}
+
+const deactivate = () => {
+    console.log("config deactivated")
+}
+
+module.exports = {
+    activate,
+    deactivate,
+    //add custom config here, such as
+
+    "ui.colorscheme": "nord",
+
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    //"editor.fontSize": "14px",
+    //"editor.fontFamily": "Monaco",
+
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "debug.neovimPath": "/usr/local/bin/nvim",
+    "oni.loadInitVim": true
+}
---
+++ init.vim
@@ -0,0 +1,19 @@
+set nocompatible            " Disable compatibility to old-time vi
+set showmatch               " Show matching brackets.
+set ignorecase              " Do case insensitive matching
+set mouse=v                 " middle-click paste with mouse
+set hlsearch                " highlight search results
+set tabstop=4               " number of columns occupied by a tab character
+set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
+set expandtab               " converts tabs to white space
+set shiftwidth=4            " width for autoindents
+set autoindent              " indent a new line the same amount as the line just typed
+set number                  " add line numbers
+set wildmode=longest,list   " get bash-like tab completions
+set cc=80                   " set an 80 column border for good coding style
+
+
+noremap h <NOP>
+noremap j <NOP>
+noremap k <NOP>
+noremap l <NOP>
